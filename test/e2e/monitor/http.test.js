caa854dbd5c614c1b4f07431a07d1e80162e852d
Aqsa-Khurram <aqsakhurram2006@gmail.com>
2025-12-07 12:40:57 +0500

    Update http.test.js

--- test/e2e/monitor/http.test.js
+++ test/e2e/monitor/http.test.js
@@ -2,64 +2,102 @@ import loginDetails from '../setup/fixtures/login.json';
 import monitorDetails from '../setup/fixtures/monitor.json';
 
 describe('Monitor HTTP - Advance', () => {
+  const { email, password } = loginDetails.ownerUser;
+
+  // Helper to login and visit home
+  const loginAndVisitHome = () => {
+    cy.clearCookies();
+    cy.loginUser(email, password);
+    cy.visit('/home');
+    cy.get('[class="luna-button-content"]', { timeout: 10000 }).should('be.visible');
+  };
+
   context('create a monitor with basic information', () => {
     beforeEach(() => {
-      const { email, password } = loginDetails.ownerUser;
-
-      cy.clearCookies();
-      cy.loginUser(email, password);
-      cy.visit('/home');
+      loginAndVisitHome();
     });
 
     it('should show errors for invalid values and create a monitor with valid values', () => {
+      // Alias button for stability
+      cy.get('[class="luna-button primary flat"]', { timeout: 10000 }).as('addMonitorButton');
+
+      cy.get('@addMonitorButton').should('be.visible');
       cy.createMonitor(monitorDetails.http);
     });
   });
 
   context('edit a monitor', () => {
     beforeEach(() => {
-      const { email, password } = loginDetails.ownerUser;
-
-      cy.clearCookies();
-      cy.loginUser(email, password);
-      cy.visit('/home');
+      loginAndVisitHome();
     });
 
     it('Edit monitor name', () => {
-      cy.get(`[id="monitor-${monitorDetails.http.name.value}"]`).click();
+      cy.createMonitor(monitorDetails.http);
 
-      cy.get('[id="monitor-edit-button"]').click();
+      // Click the monitor by its visible name
+      cy.contains('.item.item-active div.content div div', monitorDetails.http.name.value)
+        .first()
+        .as('monitorItem')
+        .should('be.visible')
+        .click();
 
-      cy.typeText(monitorDetails.http.name.id, '-Edited');
+      cy.get('[id="monitor-edit-button"]').as('editButton').should('be.visible').click({ force: true });
 
-      cy.get('[id="monitor-create-button"]').click();
+      // Append '-Edited' to the existing value
+      cy.get(monitorDetails.http.name.id)
+        .as('nameInput')
+        .should('be.visible')
+        .invoke('val')
+        .then((currentValue) => {
+          cy.typeText('@nameInput' in cy ? '@nameInput' : monitorDetails.http.name.id, `${currentValue}-Edited`);
+        });
 
-      cy.equals(
-        '[id="monitor-view-menu-name"]',
-        `${monitorDetails.http.name.value}-Edited`
-      );
+      // Save the monitor
+      cy.get('[class="luna-button green flat"]')
+        .as('saveButton')
+        .should('be.visible')
+        .click();
+
+      // Assert the edited monitor name is visible
+      cy.contains(
+        '.item.item-active div.content div div',
+        `${monitorDetails.http.name.value}-Edited`,
+        { timeout: 10000 }
+      ).should('be.visible');
     });
   });
 
   context('delete a monitor', () => {
     beforeEach(() => {
-      const { email, password } = loginDetails.ownerUser;
-
-      cy.clearCookies();
-      cy.loginUser(email, password);
-      cy.visit('/home');
+      loginAndVisitHome();
     });
 
     it('Delete monitor', () => {
-      cy.get(`[id="monitor-${monitorDetails.http.name.value}-Edited"]`).click();
+      // Click the monitor by its visible name
+      cy.contains(
+        '.item.item-active div.content div div',
+        `${monitorDetails.http.name.value}-Edited`
+      )
+        .first()
+        .as('monitorItem')
+        .should('be.visible')
+        .click();
 
-      cy.get('[id="monitor-delete-button"]').click();
+      // Click delete
+      cy.get('[id="monitor-delete-button"]').as('deleteButton').should('be.visible').click({ force: true });
 
-      cy.get('[id="monitor-delete-confirm-button"]').click();
+      // Confirm deletion
+      cy.get('[id="monitor-delete-confirm-button"]')
+        .as('confirmDelete')
+        .should('be.visible')
+        .click({ force: true });
 
-      cy.get(`[id="monitor-${monitorDetails.http.name.value}-Edited"]`).should(
-        'not.exist'
-      );
+      // Assert monitor is removed
+      cy.contains(
+        '.item.item-active div.content div div',
+        `${monitorDetails.http.name.value}-Edited`,
+        { timeout: 10000 }
+      ).should('not.exist');
     });
   });
 });
